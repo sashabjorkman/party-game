0c8a41fce214cf099f729df5990779dc18cef51f
Simon Jutwreten <simonju@kth.se>
2025-10-20 21:24:34 +0200

    Add ecs and fixed point math

---
+++ ecs.zig
@@ -0,0 +1,464 @@
+const std = @import("std");
+
+/// Entity identifier, with generation tracker.
+pub const Entity = packed struct(u64) {
+    pub const tombstone: Entity = .{
+        .identifier = ~0,
+        .generation = ~0,
+    };
+
+    identifier: u32 = 0,
+    generation: u32 = 0,
+
+    pub inline fn toBits(self: Entity) u64 {
+        return @bitCast(self);
+    }
+
+    pub inline fn fromBits(bits: u64) Entity {
+        return @bitCast(bits);
+    }
+
+    pub inline fn eq(self: Entity, other: Entity) bool {
+        return self.toBits() == other.toBits();
+    }
+
+    pub inline fn ne(self: Entity, other: Entity) bool {
+        return self.toBits() != other.toBits();
+    }
+
+    pub inline fn isTombstone(self: Entity) bool {
+        return self.identifier == tombstone.identifier;
+    }
+};
+
+/// Stores world metadata. All components must be default initializable.
+pub const Registry = struct {
+    entities: usize,
+    components: []const type,
+};
+
+pub const WorldError = error{
+    spawn_limit_exceeded,
+    dead_entity_killed,
+    dead_entity_promoted,
+    dead_entity_demoted,
+    dead_entity_inspected,
+    entity_repromoted,
+    entity_redemoted,
+    entity_lacking_component_inspected,
+};
+
+/// Stores and manipulates entities and their corresponding components.
+pub fn World(comptime registry: Registry) type {
+    for (registry.components) |C| {
+        if (@typeInfo(C) != .@"struct") {
+            @compileError("Expected struct component, found non-struct " ++ @typeName(C));
+        }
+    }
+
+    const Entities = std.bit_set.ArrayBitSet(u64, registry.entities);
+    const Signature = std.bit_set.IntegerBitSet(registry.components.len);
+
+    // TODO: fit all data exactly.
+    const buffer_size = blk: {
+        var size = 0;
+        for (registry.components) |C| {
+            size += registry.entities * @sizeOf(C) + @alignOf(C);
+        }
+        break :blk size;
+    };
+
+    const buffer_alignment = if (registry.components.len > 0) @alignOf(registry.components[0]) else 0;
+
+    const component_sizes = blk: {
+        var sizes: [registry.components.len]usize = undefined;
+
+        for (registry.components, 0..) |C, i| {
+            sizes[i] = @sizeOf(C);
+        }
+
+        break :blk sizes;
+    };
+
+    const component_alignments = blk: {
+        var alignments: [registry.components.len]usize = undefined;
+
+        for (registry.components, 0..) |C, i| {
+            alignments[i] = @alignOf(C);
+        }
+
+        break :blk alignments;
+    };
+
+    // Maps component identifiers to their corresponding buffer index.
+    const component_handles = blk: {
+        var components: [registry.components.len]usize = undefined;
+        var cursor: usize = 0;
+
+        for (&components, component_sizes, component_alignments) |*component, size, alignment| {
+            const remainder = cursor % alignment;
+
+            if (remainder != 0) {
+                cursor += alignment - remainder;
+            }
+
+            component.* = cursor;
+            cursor = cursor + registry.entities * size;
+        }
+
+        break :blk components;
+    };
+
+    return struct {
+        const SelfWorld = @This();
+
+        comptime registry: Registry = registry,
+        entities: Entities = Entities.initEmpty(),
+        generations: [registry.entities]u32 = .{0} ** registry.entities,
+        signatures: [registry.entities]Signature = .{Signature.initEmpty()} ** registry.entities,
+        buffer: [buffer_size]u8 align(buffer_alignment) = undefined,
+
+        /// Removes all entities from the world.
+        pub fn reset(self: *SelfWorld) void {
+            self.entities = Entities.initEmpty();
+            self.generations = .{0} ** registry.entities;
+            self.signatures = .{Signature.initEmpty()} ** registry.entities;
+            self.buffer = undefined;
+        }
+
+        /// Creates a new entity with default intialized components.
+        pub fn spawn(self: *SelfWorld, comptime components: []const type) !Entity {
+            const identifier = self.entities.complement().findFirstSet() orelse {
+                return WorldError.spawn_limit_exceeded;
+            };
+
+            const entity = Entity{
+                .identifier = @intCast(identifier),
+                .generation = self.generations[identifier],
+            };
+
+            self.entities.set(identifier);
+            self.signatures[identifier] = comptime getSignature(components);
+
+            inline for (components) |C| {
+                self.getArray(C)[identifier] = .{};
+            }
+
+            return entity;
+        }
+
+        /// Creates a new entity with components inferred from passed values.
+        pub fn spawnWith(self: *SelfWorld, components: anytype) !Entity {
+            const Type: type = @TypeOf(components);
+            const info = @typeInfo(Type);
+            if (info != .@"struct") {
+                @compileError("Expected tuple or struct argument, found " ++ @typeName(Type));
+            }
+
+            comptime var component_types: []const type = &.{};
+            const fields = info.@"struct".fields;
+
+            inline for (fields) |field| {
+                const component = @field(components, field.name);
+                const Component: type = @TypeOf(component);
+                component_types = component_types ++ .{Component};
+            }
+
+            const identifier = self.entities.complement().findFirstSet() orelse {
+                return WorldError.spawn_limit_exceeded;
+            };
+
+            const entity = Entity{
+                .identifier = @intCast(identifier),
+                .generation = self.generations[identifier],
+            };
+
+            const signature = comptime getSignature(component_types);
+
+            self.entities.set(identifier);
+
+            inline for (fields) |field| {
+                const component = @field(components, field.name);
+                const Component: type = @TypeOf(component);
+
+                self.getArray(Component)[identifier] = component;
+            }
+
+            self.signatures[identifier].setUnion(signature);
+
+            return entity;
+        }
+
+        /// Removes an entity.
+        pub fn kill(self: *SelfWorld, entity: Entity) !void {
+            if (!self.isAlive(entity)) {
+                return WorldError.dead_entity_killed;
+            }
+
+            self.entities.unset(entity.identifier);
+            self.generations[entity.identifier] +%= 1;
+            self.signatures[entity.identifier].mask = 0;
+        }
+
+        /// Adds default initialized components to an entity.
+        pub fn promote(self: *SelfWorld, entity: Entity, comptime components: []const type) !void {
+            if (!self.isAlive(entity)) {
+                return WorldError.dead_entity_promoted;
+            }
+
+            const signature = comptime getSignature(components);
+
+            if (self.signatures[entity.identifier].intersectWith(signature).mask != 0) {
+                return WorldError.entity_repromoted;
+            }
+
+            inline for (components) |C| {
+                self.getArray(C)[entity.identifier] = .{};
+            }
+
+            self.signatures[entity.identifier].setUnion(signature);
+        }
+
+        /// Adds components to an entity. Components should be passed as a struct.
+        pub fn promoteWith(self: *SelfWorld, entity: Entity, components: anytype) !void {
+            const Type: type = @TypeOf(components);
+            const info = @typeInfo(Type);
+
+            if (info != .@"struct") {
+                @compileError("Expected tuple or struct argument, found " ++ @typeName(Type));
+            }
+
+            comptime var component_types: []const type = &.{};
+            const fields = info.@"struct".fields;
+
+            inline for (fields) |field| {
+                const component = @field(components, field.name);
+                const Component: type = @TypeOf(component);
+                component_types = component_types ++ .{Component};
+            }
+
+            if (!self.isAlive(entity)) {
+                return WorldError.dead_entity_promoted;
+            }
+
+            const signature = comptime getSignature(component_types);
+
+            if (self.signatures[entity.identifier].intersectWith(signature).mask != 0) {
+                return WorldError.entity_repromoted;
+            }
+
+            inline for (fields) |field| {
+                const component = @field(components, field.name);
+                const Component: type = @TypeOf(component);
+
+                self.getArray(Component)[entity.identifier] = component;
+            }
+
+            self.signatures[entity.identifier].setUnion(signature);
+        }
+
+        /// Removes components from an entity.
+        pub fn demote(self: *SelfWorld, entity: Entity, comptime components: []const type) !void {
+            const signature = comptime getSignature(components);
+
+            if (!self.isAlive(entity)) {
+                return WorldError.dead_entity_demoted;
+            }
+
+            if (self.signatures[entity.identifier].complement().intersectWith(signature).mask != 0) {
+                return WorldError.entity_redemoted;
+            }
+
+            self.signatures[entity.identifier].setIntersection(signature.complement());
+        }
+
+        /// Inspects a component from an entity. Prefer using `query()`.
+        pub fn inspectConst(self: *const SelfWorld, entity: Entity, comptime C: type) !*const C {
+            if (!isAlive(self, entity)) {
+                return WorldError.dead_entity_inspected;
+            }
+
+            if (self.signatures[entity.identifier].intersectWith(comptime getTag(C)).mask == 0) {
+                return WorldError.entity_lacking_component_inspected;
+            }
+
+            return &self.getArrayConst(C)[entity.identifier];
+        }
+
+        /// Inspects a component from an entity. Prefer using `query()`.
+        pub fn inspect(self: *SelfWorld, entity: Entity, comptime C: type) !*C {
+            if (!isAlive(self, entity)) {
+                return WorldError.dead_entity_inspected;
+            }
+
+            if (self.signatures[entity.identifier].intersectWith(comptime getTag(C)).mask == 0) {
+                return WorldError.entity_lacking_component_inspected;
+            }
+
+            return &self.getArray(C)[entity.identifier];
+        }
+
+        /// Returns true if entity currently exists.
+        pub fn isAlive(self: *const SelfWorld, entity: Entity) bool {
+            if (!self.entities.isSet(entity.identifier)) {
+                return false;
+            }
+
+            return entity.generation == self.generations[entity.identifier];
+        }
+
+        /// Returns true if an entity exists and matches component signature.
+        pub fn hasSignature(self: *const SelfWorld, entity: Entity, comptime include: []const type, comptime exclude: []const type) bool {
+            if (!self.entities.isSet(entity.identifier)) {
+                return false;
+            }
+
+            const included = comptime getSignature(include);
+            const excluded = comptime getSignature(exclude);
+            const signature = self.signatures[entity.identifier];
+
+            return signature.intersectWith(included).xorWith(signature.intersectWith(excluded)).eql(included);
+        }
+
+        /// Retrieves all entities matching component signature.
+        /// Spawning, killing, promoting, and demoting entities may invalidate queries.
+        pub fn queryConst(self: *const SelfWorld, comptime include: []const type, comptime exclude: []const type) Query(include, exclude, false) {
+            return .init(self);
+        }
+
+        /// Retrieves all entities matching component signature.
+        /// Spawning, killing, promoting, and demoting entities may invalidate queries.
+        pub fn query(self: *SelfWorld, comptime include: []const type, comptime exclude: []const type) Query(include, exclude, true) {
+            return .init(self);
+        }
+
+        fn getArrayConst(self: *const SelfWorld, comptime Component: type) *const [registry.entities]Component {
+            const identifier = comptime getIdentifier(Component);
+            const handle = comptime component_handles[identifier];
+            const ptr: *const anyopaque = self.buffer[handle..];
+
+            return @ptrCast(@alignCast(ptr));
+        }
+
+        fn getArray(self: *SelfWorld, comptime Component: type) *[registry.entities]Component {
+            const identifier = comptime getIdentifier(Component);
+            const handle = comptime component_handles[identifier];
+            const ptr: *anyopaque = self.buffer[handle..];
+
+            return @ptrCast(@alignCast(ptr));
+        }
+
+        fn getIdentifier(comptime Component: type) usize {
+            comptime {
+                for (registry.components, 0..) |C, i| {
+                    if (C == Component) {
+                        return i;
+                    }
+                }
+
+                @compileError("Invalid component: " ++ @typeName(Component));
+            }
+        }
+
+        fn getTag(comptime Component: type) Signature {
+            comptime {
+                for (registry.components, 0..) |C, i| {
+                    if (C == Component) {
+                        var mask = Signature.initEmpty();
+                        mask.set(i);
+                        return mask;
+                    }
+                }
+
+                @compileError("Invalid component: " ++ @typeName(Component));
+            }
+        }
+
+        fn getSignature(comptime components: []const type) Signature {
+            comptime {
+                var mask = Signature.initEmpty();
+
+                for (components) |c| {
+                    mask.setUnion(getTag(c));
+                }
+
+                return mask;
+            }
+        }
+
+        /// An iterator over entities with a specific component signature.
+        /// Included components refers to components that the entity must have.
+        /// Excluded components refers to components that the entity must not have.
+        fn Query(comptime include: []const type, comptime exclude: []const type, comptime mutable: bool) type {
+            comptime {
+                for (include) |I| {
+                    for (exclude) |E| {
+                        if (I == E) {
+                            @compileError("Query both includes and excludes " ++ @typeName(I));
+                        }
+                    }
+                }
+            }
+
+            return struct {
+                const SelfQuery = @This();
+                const QueryWorld = if (mutable) *SelfWorld else *const SelfWorld;
+
+                /// Wraps an entity in the query context, improving safety and performance.
+                const QueriedEntity = struct {
+                    entity: Entity,
+                };
+
+                world: QueryWorld,
+                iterator: Entities.Iterator(.{}),
+
+                fn init(world: QueryWorld) SelfQuery {
+                    return SelfQuery{
+                        .world = world,
+                        .iterator = world.entities.iterator(.{}),
+                    };
+                }
+
+                /// Retrieves the next entity.
+                pub fn next(self: *SelfQuery) ?QueriedEntity {
+                    const included = comptime getSignature(include);
+                    const excluded = comptime getSignature(exclude);
+
+                    while (self.iterator.next()) |i| {
+                        const signature: Signature = self.world.signatures[i];
+
+                        if (signature.intersectWith(included).xorWith(signature.intersectWith(excluded)).eql(included)) {
+                            const entity = Entity{
+                                .identifier = @intCast(i),
+                                .generation = self.world.generations[i],
+                            };
+
+                            return QueriedEntity{ .entity = entity };
+                        }
+                    }
+
+                    return null;
+                }
+
+                /// Retrieves component data of a queried entity.
+                pub fn get(self: *const SelfQuery, entity: QueriedEntity, comptime C: type) if (mutable) *C else *const C {
+                    comptime {
+                        for (include) |c| {
+                            if (c == C) {
+                                break;
+                            }
+                        } else {
+                            @compileError("Invalid component: " ++ @typeName(C));
+                        }
+                    }
+
+                    if (mutable) {
+                        return &self.world.getArray(C)[entity.entity.identifier];
+                    } else {
+                        return &self.world.getArrayConst(C)[entity.entity.identifier];
+                    }
+                }
+            };
+        }
+    };
+}
---
+++ fixed_point.zig
@@ -0,0 +1,103 @@
+pub const F32 = packed struct(i32) {
+    bits: i32,
+
+    /// Initialize a fixed point number with inferred arguments.
+    pub inline fn init(initializer: anytype) F32 {
+        return @bitCast(infer(initializer));
+    }
+
+    /// Returns the sum of two fixed point numbers.
+    pub inline fn add(augend: F32, addend: anytype) F32 {
+        return @bitCast(augend.bits + infer(addend));
+    }
+
+    /// Returns the difference of two fixed point numbers.
+    pub inline fn sub(minuend: F32, subtrahend: anytype) F32 {
+        return @bitCast(minuend.bits - infer(subtrahend));
+    }
+
+    /// Returns the product of two fixed point numbers.
+    pub inline fn mul(multiplicand: F32, multiplier: anytype) F32 {
+        return switch (@typeInfo(@TypeOf(multiplier))) {
+            inline .int, .comptime_int => @bitCast(multiplicand.bits * @as(i32, multiplier)),
+            inline else => @bitCast(@as(i32, @intCast((@as(i64, multiplicand.bits) * @as(i64, infer(multiplier))) >> 16))),
+        };
+    }
+
+    /// Returns the quotient of two fixed point numbers.
+    pub inline fn div(dividend: F32, divisor: anytype) F32 {
+        return switch (@typeInfo(@TypeOf(divisor))) {
+            inline .int, .comptime_int => @bitCast(@divTrunc(dividend.bits, @as(i32, divisor))),
+            inline else => @bitCast(@as(i32, @intCast(@divTrunc(@as(i64, dividend.bits) << 16, @as(i64, infer(divisor)))))),
+        };
+    }
+
+    /// Returns the absoulute value of a fixed point number.
+    pub inline fn abs(self: F32) F32 {
+        return @bitCast(@as(i32, @intCast(@abs(self.bits))));
+    }
+
+    /// Returns the integer square of a fixed point number.
+    pub inline fn sqr(self: F32) F32 {
+        return mul(self, self);
+    }
+
+    /// Source: Jonathan Hallström.
+    /// Returns the square root of a fixed point number.
+    pub inline fn sqrt(self: F32) F32 {
+        if (self.bits < 0) {
+            unreachable;
+        }
+
+        const n = @as(i64, self.bits) << 16;
+        var x: i32 = infer(@sqrt(asFloat(self))).bits + 1;
+
+        if (@as(i64, x) * @as(i64, x) > n) {
+            x -= 1;
+        }
+
+        if (@as(i64, x + 1) * @as(i64, x + 1) <= n) {
+            x += 1;
+        }
+
+        return @bitCast(x);
+    }
+
+    /// Compares two fixed point numbers.
+    pub inline fn cmp(a: F32, b: anytype, comptime op: enum { eq, ne, gt, ge, lt, le }) bool {
+        return switch (op) {
+            .eq => a.bits == infer(b),
+            .ne => a.bits != infer(b),
+            .gt => a.bits > infer(b),
+            .ge => a.bits >= infer(b),
+            .lt => a.bits < infer(b),
+            .le => a.bits <= infer(b),
+        };
+    }
+
+    /// Returns the floored integer representation of a fixed point number.
+    pub inline fn asInt(self: F32) i16 {
+        return @truncate(self.bits >> 16);
+    }
+
+    /// Returns the floating point representation of a fixed point number.
+    pub inline fn asFloat(self: F32) f32 {
+        return @as(f32, @floatFromInt(self.bits)) / (1 << 16);
+    }
+
+    /// Safely casts a value to its fixed point representation.
+    pub inline fn infer(value: anytype) i32 {
+        const Type = @TypeOf(value);
+        const info = @typeInfo(Type);
+
+        if (Type == F32) {
+            return value.bits;
+        }
+
+        return switch (info) {
+            inline .int, .comptime_int => @as(i32, @as(i16, value)) << 16,
+            inline .float, comptime_float => @as(i32, @intFromFloat(value * (1 << 16))),
+            inline else => @compileError("Expected fixed point, floating point, or integer type, but got type " ++ @typeName(Type)),
+        };
+    }
+};
